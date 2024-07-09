1b3fcf630df9da2f842f32d2e834f859b78a02d0
Saiful <76849761+saifulIslamSourceCode@users.noreply.github.com>
2024-07-10 00:03:49 +0600

    Practice Angular Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from "./hello-world/hello-world.component";
+import { HelloWorld2Component } from "./hello-world-2/hello-world-2.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent, HelloWorld2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/hello-world-2/hello-world-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-world-2.component.html',
+  styleUrl: './hello-world-2.component.css'
+})
+export class HelloWorld2Component {
+
+}
